c86edef0673684ef2ba07ec031689e21a508c14d
zeiad-a7med <zeiadahmed194@gmail.com>
2025-02-09 18:58:56 +0200

    first commit

---
+++ M-Commerce/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  M-Commerce
+//
+//  Created by Zeiad on 09/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ M-Commerce/M_CommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  M_CommerceApp.swift
+//  M-Commerce
+//
+//  Created by Zeiad on 09/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct M_CommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
