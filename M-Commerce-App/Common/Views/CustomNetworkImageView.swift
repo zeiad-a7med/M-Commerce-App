437e64b8bb622e1130acbf8d1835a51ca915b4cf
zeiad-a7med <zeiadahmed194@gmail.com>
2025-02-12 01:05:09 +0200

    add xcode15 project rest files

---
+++ M-Commerce-App/Common/Views/CustomNetworkImageView.swift
@@ -0,0 +1,63 @@
+//
+//  CustomNetworkImageView.swift
+//  M-Commerce
+//
+//  Created by Zeiad on 09/02/2025.
+//
+import SwiftUI
+import Combine
+
+class ImageLoader: ObservableObject {
+    @Published var image: UIImage?
+
+    private var cancellable: AnyCancellable?
+    private static var imageCache = NSCache<NSURL, UIImage>()
+
+    func load(from url: URL) {
+        // Check cache first
+        if let cachedImage = Self.imageCache.object(forKey: url as NSURL) {
+            DispatchQueue.main.async {
+                self.image = cachedImage
+            }
+            return
+        }
+
+        // Download only if not cached
+        cancellable = URLSession.shared.dataTaskPublisher(for: url)
+            .map { UIImage(data: $0.data) }
+            .replaceError(with: nil)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] image in
+                guard let self = self, let image = image else { return }
+                Self.imageCache.setObject(image, forKey: url as NSURL) // Cache the image
+                self.image = image
+            }
+    }
+
+    deinit {
+        cancellable?.cancel()
+    }
+}
+
+struct CustomNetworkImageView: View {
+    
+    let url: URL
+    @StateObject private var loader = ImageLoader()
+    
+    var body: some View {
+        Group {
+            if let image = loader.image {
+                Image(uiImage: image)
+                    .resizable()
+                    
+            } else {
+                ShimmerView(width: 150, height: 150)
+            }
+        }
+        .onAppear {
+            if loader.image == nil { // Load image only if not already loaded
+                loader.load(from: url)
+            }
+        }
+    }
+}
--- M-Commerce-App/ContentView.swift
+++ M-Commerce-App/ContentView.swift
@@ -1,61 +1,28 @@
 //
 //  ContentView.swift
-//  M-Commerce-App
+//  M-Commerce
 //
-//  Created by Andrew Emad on 12/02/2025.
+//  Created by Zeiad on 09/02/2025.
 //
 
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
+        TabView {
+            HomePageView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+            FavoritesView()
+                .tabItem {
+                    Label("Favorites", systemImage: "heart.fill")
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
+                .badge(10)
         }
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
