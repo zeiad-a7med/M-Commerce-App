ceeb973ade368a91af700a884a34f8922ce9689d
zeiad ahmed <152458062+zeiad-a7med@users.noreply.github.com>
2025-02-12 14:09:24 +0200

    Revert "Graphql setup"

--- ShopifyAPI/Sources/Schema/Interfaces/HasMetafields.graphql.swift
+++
@@ -1,9 +0,0 @@
-// @generated
-// This file was automatically generated and should not be edited.
-
-import ApolloAPI
-
-public extension Interfaces {
-  /// Represents information about the metafields associated to the specified resource.
-  static let HasMetafields = ApolloAPI.Interface(name: "HasMetafields", keyFields: nil)
-}
