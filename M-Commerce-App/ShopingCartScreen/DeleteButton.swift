893b7281cbaf083f48da2c090360c03e275990a5
Andrew Emad <andrew14emad@gmail.com>
2025-02-12 02:25:50 -0800

    Added intial Shopping Cart View

---
+++ M-Commerce-App/ShopingCartScreen/DeleteButton.swift
@@ -0,0 +1,29 @@
+//
+//  deleteButton.swift
+//  ShoppingCart
+//
+//  Created by Andrew Emad on 11/02/2025.
+//
+
+import SwiftUI
+
+struct DeleteButton: View {
+    var body: some View {
+        Circle()
+            .frame(width: 25,height: 25)
+            .foregroundStyle(Color(#colorLiteral(red: 0.9997449517, green: 0.8575152755, blue: 0.8603120446, alpha: 1)))
+            .overlay{
+                Button("", systemImage: "xmark.bin") {
+                    
+                }.frame(width: 3,height: 3)
+                    .foregroundStyle(.red)
+                    .offset(x:3.7,y:0)
+                    .controlSize(.small)
+                    
+            }
+    }
+}
+
+#Preview {
+    DeleteButton()
+}
---
+++ M-Commerce-App/ShopingCartScreen/imageCard.swift
@@ -0,0 +1,50 @@
+//
+//  SwiftUIView.swift
+//  ShoppingCart
+//
+//  Created by Andrew Emad on 10/02/2025.
+//
+
+import SwiftUI
+
+struct imageCard: View {
+    @State var img : String
+    var body: some View {
+        AsyncImage(url: URL(string: img)) { phase in
+                switch phase {
+                case .empty:
+                    ProgressView()
+                        
+                        .frame(width: 325 , height: 100)
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                        .containerRelativeFrame(.horizontal, alignment: .center)
+                        
+                case .success(let image):
+                
+                        image
+                           .resizable()
+                           .frame(width: 350 , height: 120)
+                           .clipShape(RoundedRectangle(cornerRadius: 15))
+                           .containerRelativeFrame(.horizontal, alignment: .center)
+                           .aspectRatio(contentMode: .fit)
+               
+                case .failure:
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 15)
+                            .frame(width: 330 , height: 100)
+                            .foregroundStyle(.gray)
+                            .containerRelativeFrame(.horizontal, alignment: .center)
+                        Text("Failed to load image")
+                            .foregroundStyle(.white)
+                    }
+                @unknown default:
+                    EmptyView()
+                }
+            }
+    }
+}
+
+#Preview {
+    imageCard(img: "https://burst.shopifycdn.com/photos/black-sneakers-with-white-sole.jpg?width=4460&height=4460&exif=0&iptc=0"
+    )
+}
