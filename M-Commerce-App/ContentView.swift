b9586f0efc75091a2a1ab27405afb4f3eedbc04f
zeiad-a7med <zeiadahmed194@gmail.com>
2025-02-15 19:05:40 +0200

    fix modelContainer for previews

--- M-Commerce-App/ContentView.swift
+++ M-Commerce-App/ContentView.swift
@@ -27,18 +27,6 @@ struct ContentView: View {
 }
 
 #Preview {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Product.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
     ContentView()
-        .modelContainer(sharedModelContainer)
+        .modelContainer(for: Product.self, inMemory: true)
 }
--- M-Commerce-App/M_Commerce_AppApp.swift
+++ M-Commerce-App/M_Commerce_AppApp.swift
@@ -4,36 +4,58 @@
 //
 //  Created by Andrew Emad on 12/02/2025.
 //
+//
+//  M_Commerce_AppApp.swift
+//  M-Commerce-App
+//
+//  Created by Andrew Emad on 12/02/2025.
+//
+//
+//  M_Commerce_AppApp.swift
+//  M-Commerce-App
+//
+//  Created by Andrew Emad on 12/02/2025.
+//
 
 import SwiftData
 import SwiftUI
 
 @main
 struct M_Commerce_AppApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Product.self
-        ])
-        let modelConfiguration = ModelConfiguration(
-            schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(
-                for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
+            let isSimulator = TARGET_OS_SIMULATOR != 0
+            let sharedModelContainer: ModelContainer? = isSimulator ? {
+                let schema = Schema([Product.self])
+                let modelConfiguration = ModelConfiguration(
+                    schema: schema, isStoredInMemoryOnly: false)
+
+                do {
+                    return try ModelContainer(
+                        for: schema, configurations: [modelConfiguration])
+                } catch {
+                    fatalError("Could not create ModelContainer: \(error)")
+                }
+            }() : nil
+            
             ZStack {
                 ContentView()
                 SnackbarView()
                     .environmentObject(SnackbarManager.shared)
             }
-            
+            .applyModelContainer(sharedModelContainer)
         }
-        .modelContainer(sharedModelContainer)
+    }
+}
+
+// Extension to conditionally apply modelContainer
+extension View {
+    @ViewBuilder
+    func applyModelContainer(_ container: ModelContainer?) -> some View {
+        if let container = container {
+            self.modelContainer(container)
+        } else {
+            self
+        }
     }
 }
