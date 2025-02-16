a183b873ef2759cd28ef995d6c1223da8822073c
zeiad-a7med <zeiadahmed194@gmail.com>
2025-02-17 00:16:14 +0200

    add constants file

---
+++ M-Commerce-App/Constanst/Constants.swift
@@ -0,0 +1,41 @@
+//
+//  Constants.swift
+//  M-Commerce-App
+//
+//  Created by Zeiad on 15/02/2025.
+//
+
+import Foundation
+class Constants {
+    static let countries: [Country] = [
+        Country(name: "Egypt", code: "+20", flag: "🇪🇬"),
+        Country(name: "Saudi Arabia", code: "+966", flag: "🇸🇦"),
+        Country(name: "United Arab Emirates", code: "+971", flag: "🇦🇪"),
+        Country(name: "Qatar", code: "+974", flag: "🇶🇦"),
+        Country(name: "Kuwait", code: "+965", flag: "🇰🇼"),
+        Country(name: "Bahrain", code: "+973", flag: "🇧🇭"),
+        Country(name: "Oman", code: "+968", flag: "🇴🇲"),
+        Country(name: "Lebanon", code: "+961", flag: "🇱🇧"),
+        Country(name: "Jordan", code: "+962", flag: "🇯🇴"),
+        Country(name: "Iraq", code: "+964", flag: "🇮🇶"),
+        Country(name: "Morocco", code: "+212", flag: "🇲🇦"),
+        Country(name: "Algeria", code: "+213", flag: "🇩🇿"),
+        Country(name: "Tunisia", code: "+216", flag: "🇹🇳"),
+        Country(name: "Sudan", code: "+249", flag: "🇸🇩"),
+        Country(name: "Syria", code: "+963", flag: "🇸🇾"),
+        Country(name: "Palestine", code: "+970", flag: "🇵🇸"),
+        Country(name: "Libya", code: "+218", flag: "🇱🇾"),
+        Country(name: "Mauritania", code: "+222", flag: "🇲🇷"),
+        Country(name: "Yemen", code: "+967", flag: "🇾🇪"),
+        Country(name: "United States", code: "+1", flag: "🇺🇸"),
+        Country(name: "Canada", code: "+1", flag: "🇨🇦"),
+        Country(name: "United Kingdom", code: "+44", flag: "🇬🇧"),
+        Country(name: "Australia", code: "+61", flag: "🇦🇺"),
+        Country(name: "India", code: "+91", flag: "🇮🇳"),
+        Country(name: "Germany", code: "+49", flag: "🇩🇪"),
+        Country(name: "France", code: "+33", flag: "🇫🇷"),
+        Country(name: "Italy", code: "+39", flag: "🇮🇹"),
+        Country(name: "Spain", code: "+34", flag: "🇪🇸"),
+        Country(name: "Brazil", code: "+55", flag: "🇧🇷")
+    ]
+}
