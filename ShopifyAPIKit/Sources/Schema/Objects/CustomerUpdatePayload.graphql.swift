e05f297387e7209f057163dd7fc9956dc1eb4c9b
zeiad-a7med <zeiadahmed194@gmail.com>
2025-02-18 01:16:16 +0200

    finish updating profile functionality

---
+++ ShopifyAPIKit/Sources/Schema/Objects/CustomerUpdatePayload.graphql.swift
@@ -0,0 +1,13 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+import ApolloAPI
+
+public extension Objects {
+  /// Return type for `customerUpdate` mutation.
+  static let CustomerUpdatePayload = ApolloAPI.Object(
+    typename: "CustomerUpdatePayload",
+    implementedInterfaces: [],
+    keyFields: nil
+  )
+}
