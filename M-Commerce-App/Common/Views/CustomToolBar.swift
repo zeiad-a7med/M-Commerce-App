5add0a1abf2daa75bc19cf525953366ed8769123
zeiad-a7med <zeiadahmed194@gmail.com>
2025-02-24 10:14:56 +0200

    fix counters publishers

--- M-Commerce-App/Common/Views/CustomToolBar.swift
+++ M-Commerce-App/Common/Views/CustomToolBar.swift
@@ -6,27 +6,28 @@
 //
 
 import SwiftUI
+
 struct CustomToolBar: View {
+    @State var favoriteCount: Int = 0
+    @State var cartCount: Int = 0
+    @ObservedObject var authManager = AuthManager.shared
+    @ObservedObject var favoritesManager = FavoritesManager.shared
     var body: some View {
         HStack {
             ButtonView(
                 imageSystemName: ThemeManager.cartImg,
-                itemCounter: AuthManager.shared
-                    .applicationUser?.cart?
-                    .totalQuantity ?? 0)
+                itemCounter: authManager.applicationUser?.cart?.totalQuantity ?? 0
+            )
             .onTapGesture {
                 NavigationManager.shared.push(.cart)
             }
             ButtonView(
                 imageSystemName: ThemeManager
                     .favouriteImg,
-                itemCounter: FavoritesManager.shared.favorites.count)
+                itemCounter: favoritesManager.favorites.count
+            )
             .onTapGesture {
                 NavigationManager.shared.push(.favorites)
-            }
-            .onAppear {
-                //                                        favoriteCount =
-                //                                            FavoritesManager.shared.favorites.count
             }
         }
     }
